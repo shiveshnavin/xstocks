6ac87e7d92b8f40d9ffa9416350e02087f43e859
Shivesh Navin <shiveshnavin@gmail.com>
2023-04-09 18:58:37 +0530

    Add multiple scips

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     getProfile();
     initializeScipFinder()
+    renderSelectedItems()
 });
 
 function getHost() {
@@ -63,9 +64,15 @@ function login() {
 
 
 
+var selectedItems = [];
+
 function initializeScipFinder() {
+
     const input = document.getElementById('stockName');
     const suggestionsList = document.getElementById("suggestions");
+    if (getLocalStorageItem('scip')) {
+        input.value = getLocalStorageItem('scip')
+    }
 
     // Listen to input events on the text input
     input.addEventListener('input', async function () {
@@ -88,8 +95,83 @@ function initializeScipFinder() {
 
             suggestion.addEventListener("click", () => {
                 input.value = item?.tradingsymbol;
+                setLocalStorageItem('scip', input.value)
                 suggestionsList.innerHTML = "";
+                addSelectedItem(item?.tradingsymbol);
+
             });
         });
     });
+}
+
+
+function addSelectedItem(item) {
+    if (!selectedItems.some((selectedItem) => selectedItem.tradingsymbol === item)) {
+        selectedItems.push({ tradingsymbol: item, enabled: true });
+        renderSelectedItems();
+    }
+}
+
+function removeSelectedItem(item) {
+    const index = selectedItems.findIndex((selectedItem) => selectedItem.tradingsymbol === item);
+    if (index >= 0) {
+        selectedItems.splice(index, 1);
+        renderSelectedItems();
+    }
+}
+
+function toggleSelectedItem(item) {
+    const selectedItem = selectedItems.find((selectedItem) => selectedItem.tradingsymbol === item);
+    if (selectedItem) {
+        selectedItem.enabled = !selectedItem.enabled;
+        renderSelectedItems();
+    }
+}
+
+function renderSelectedItems() {
+    if (selectedItems && selectedItems.length > 0)
+        setLocalStorageItem('selected_scips', JSON.stringify(selectedItems))
+    if (getLocalStorageItem('selected_scips')) {
+        let str = getLocalStorageItem('selected_scips')
+        selectedItems = JSON.parse(getLocalStorageItem('selected_scips'))
+    }
+
+
+
+    const selectedItemsList = document.getElementById("selected-items");
+    selectedItemsList.innerHTML = ''
+
+    selectedItems.forEach((selectedItem) => {
+        const item = document.createElement("li");
+        item.classList.add("selected-item");
+        item.textContent = selectedItem.tradingsymbol;
+
+        const buttons = document.createElement("div");
+        buttons.classList.add("buttons");
+
+        const toggleButton = document.createElement("span");
+        toggleButton.classList.add("action", "show-hide");
+        toggleButton.style.color = selectedItem.enabled ? "#00f" : "#f00";
+        toggleButton.textContent = selectedItem.enabled ? "SHOW" : "HIDE";
+        toggleButton.addEventListener("click", () => {
+            toggleSelectedItem(selectedItem.tradingsymbol);
+            renderSelectedItems()
+        });
+        buttons.appendChild(toggleButton);
+
+        const deleteButton = document.createElement("span");
+        deleteButton.classList.add("action", "delete");
+        deleteButton.style.color = "#f00";
+        deleteButton.textContent = "DELETE";
+        deleteButton.addEventListener("click", () => {
+            removeSelectedItem(selectedItem.tradingsymbol);
+            renderSelectedItems()
+        });
+        buttons.appendChild(deleteButton);
+
+        item.appendChild(buttons);
+
+        selectedItemsList.appendChild(item);
+    });
+
 }
